3834c35b8007d4b5797072164808371b65b54f4a
Warren Kushner <warren.kushner@gmail.com>
2015-07-25 19:05:32 +0000

    removed comments

--- test/test-server.js
+++ test/test-server.js
@@ -5,7 +5,6 @@ global.environment = 'test';
 var server = require('../server.js');
 var Item = require('../models/item');
 var seed = require('../db/seed.js');
-//var request = require('request');
 var should = chai.should();
 var app = server.app;
 
@@ -32,23 +31,10 @@ describe('Shopping List', function() {
             .post('/items')
             .send({'name':'Kale'})
             .end(function(err,res){
-              // err.should.equal(err, null);
                 res.should.have.status(201);
-                //res.should.be.json;
-                //res.body.should.be.a('object');
-              //  res.body.should.have.property('name');
                 res.body.should.have.property('_id');
-            //    res.body.name.should.be.a('string');
-              //  res.body._id.should.be.a('string');
                 res.body.name.should.equal('Kale');
-                // storage.items.should.be.a('array'); What should I do with these tests?
-                // storage.items.should.have.length(4);
-                // storage.items[3].should.be.a('object');
-                // storage.items[3].should.have.property('_id');
-                // storage.items[3].should.have.property('name');
-                // storage.items[3].id.should.be.a('number');
-                // storage.items[3].name.should.be.a('string');
-                // storage.items[3].name.should.equal('Kale');
+                
                 done();
             });
     });
